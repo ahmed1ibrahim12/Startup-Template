9f514594459f6e40f59b0afaf9c2e677227b039c
ahmed1ibrahim12 <ahmed1ibahim12@gmail.com>
2023-08-15 02:10:49 +0300

    responsive

---
+++ JS/style.js
@@ -0,0 +1,13 @@
+var x = document.getElementById("absoulote");
+
+function show(){
+    if (x.style.display=="none"){
+        x.style.display="flex !important";
+    } else {
+        x.style.display="none";
+    }
+}
+
+function hide(){
+        x.style.display="none";
+}
